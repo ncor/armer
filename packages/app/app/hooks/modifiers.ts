6b2f06ccc35ab58462e3d640f7e451a6586b6871
ensi <66754841+ncor@users.noreply.github.com>
2023-11-23 13:12:07 +0300

    migrated

--- packages/app/app/hooks/modifiers.ts
+++ packages/app/app/hooks/modifiers.ts
@@ -1,9 +1,31 @@
 import { useContext } from "react";
-import { ModifiersContext } from "../contexts/modifiers";
+import { IModifiersContext, ModifiersContext } from "../contexts/modifiers";
 
 
-export default function useKeystroke() {
+export default function useModifiers() {
     const modifiers = useContext(ModifiersContext);
 
-    return modifiers;
+    const applyModifiers = (key: string) => {
+        if (modifiers.isOnlyShiftPressed)
+            return key;
+
+        if (modifiers.isShiftPressed)
+            key = '⇧' + key;
+        if (modifiers.isCtrlPressed)
+            key = '⌃' + key;
+        if (modifiers.isAltPressed)
+            key = '⌥' + key;
+        if (modifiers.isMetaPressed)
+            key = '⌘' + key;
+    
+        return key;
+    }
+
+    const applyCase = (key: string) => {
+        return modifiers.isShiftPressed || modifiers.isCapsActive
+            ? key
+            : key.toLowerCase();
+    }
+
+    return { modifiers, applyModifiers, applyCase };
 }
