dc235b09925a5c3ca4d970fd5b29cbceb349c656
ensi <66754841+ncor@users.noreply.github.com>
2023-11-23 13:14:20 +0300

    refactored, new features

--- packages/app/app/components/CasterInputBox.tsx
+++ packages/app/app/components/CasterInputBox.tsx
@@ -1,57 +1,114 @@
-'use client';
+"use client";
 
-import { F_KEYS, KEYMAP, MODIFIERS } from "@/lib/keyboard/constants";
-import { IModifiersContext, ModifiersContext } from "../contexts/modifiers"
-import { useContext, useEffect, useState } from "react"
+import {
+    CAPS_LOCK,
+    DELETE_KEYS,
+    F_KEYS,
+    KEYMAP,
+    MODIFIERS,
+    RESET_KEYS,
+    SHIFT_EXTENSIONS,
+    UNPRINTABLE_KEYS,
+} from "@/lib/keyboard/constants";
+import { useEffect, useState } from "react";
 import useKeystroke from "../hooks/keystroke";
 import useFade from "../hooks/fade";
-
-
-const applyModifiers = (modifiers: IModifiersContext, key: string) => {
-    if (modifiers.isShiftPressed)
-        key = '⇧' + key;
-    else if (modifiers.isCtrlPressed)
-        key = '⌃' + key;
-    else if (modifiers.isAltPressed)
-        key = '⌥' + key;
-    else if (modifiers.isMetaPressed)
-        key = '⌘' + key;
-
-    return key;
-}
+import useModifiers from "../hooks/modifiers";
+import { motion } from "framer-motion";
 
 
 export default function CasterInputBox() {
-    const isFaded = useFade();
+    const { isFaded, toggleFade } = useFade();
     const keystroke = useKeystroke();
-    const modifiers = useContext(ModifiersContext);
-    const [ input, setInput ] = useState<string>('');
+    const { modifiers, applyModifiers, applyCase } = useModifiers();
+    const [input, setInput] = useState<string>('');
+    const [deletedInput, setDeletedInput] = useState<string>('');
+
+    const reset = () => {
+        setInput('');
+        setDeletedInput('');
+    };
 
     useEffect(() => {
         if (!keystroke) return;
 
-        if (isFaded) return setInput('');
+        if (isFaded) return reset();
 
-        if (!MODIFIERS.includes(keystroke.key)) {
-            let { key } = keystroke;
+        let { key } = keystroke;
 
-            if (!F_KEYS.includes(key)) {
-                if (KEYMAP[key] != undefined) {
-                    key = KEYMAP[key];
+        if (
+            key != CAPS_LOCK &&
+            !MODIFIERS.includes(key) &&
+            !UNPRINTABLE_KEYS.includes(key)
+        ) {
+            const isDeleteKey = DELETE_KEYS.includes(key);
+            const isResetKey = RESET_KEYS.includes(key);
+
+            if (!F_KEYS.includes(key) && !isDeleteKey) {
+                if (SHIFT_EXTENSIONS[key] && modifiers.isOnlyShiftPressed) {
+                    key = SHIFT_EXTENSIONS[key];
                 } else {
-                    key = modifiers.isShiftPressed ? key : key.toLowerCase();
+                    key = KEYMAP[key] || applyCase(key);
+                    key = applyModifiers(key);
                 }
             }
 
-            key = applyModifiers(modifiers, key);
-            
-            setInput(prevInput => prevInput + key);
+            setInput(prevInput => {
+                if (isResetKey) {
+                    setDeletedInput('');
+                    return key;
+                }
+
+                if (isDeleteKey) {
+                    setDeletedInput(prev => {
+                        if (prevInput.length > 0) {
+                            return prevInput[prevInput.length - 1] + prev;
+                        } else {
+                            toggleFade(true);
+                            return "";
+                        }
+                    });
+                    
+                    return prevInput.slice(0, prevInput.length - 1);
+                } else {
+                    setDeletedInput(prev => prev.slice(key.length));
+                }
+
+                return prevInput + key;
+            });
         }
     }, [ keystroke, isFaded ]);
 
-    return <div
-        className="overflow-hidden w-full h-[120px] flex justify-end text-[5em] px-4 pt-[52px] pb-[44px] mb-[2px] bg-black bg-opacity-75 rounded-t-3xl"
-    >
-        <div className="mx-auto whitespace-nowrap">{ input }</div>
-    </div>
+    const fadeStyles = isFaded ? 'opacity-0' : '';
+
+    /**
+     * Based on "i" symbol width.
+     */
+    if (input.length > 24)
+        setInput(input.slice(input.length - 24));
+
+    return (
+        <div className={
+            `overflow-hidden w-full min-h-[80px] flex justify-end text-5xl p-4 px-6 bg-black bg-opacity-75 rounded-3xl 
+            transition-all duration-100 ${fadeStyles}`
+        }>
+            <div className="flex mx-auto whitespace-nowrap">
+                {
+                    input && [ ...input ].map((stroke, i) =>
+                        <motion.div
+                            key={ i }
+                            animate={{ y: [ 5, 0 ], opacity: [ 0, 1 ] }}
+                            transition={{ duration: 0.1, delay: 0.1 }}
+                        >
+                            { stroke }
+                        </motion.div>
+                    )
+                }
+                {
+                    deletedInput.length > 0 &&
+                    <p className="opacity-25">{ deletedInput }</p>
+                }
+            </div>
+        </div>
+    );
 }
