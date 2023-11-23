a567a5c03106237d9170155554f4425adb2069be
ensi <66754841+ncor@users.noreply.github.com>
2023-11-23 13:13:47 +0300

    changed to optional mount

--- packages/app/app/components/CasterModifiersItem.tsx
+++ packages/app/app/components/CasterModifiersItem.tsx
@@ -13,7 +13,11 @@ export default function CasterModifiersItem({
 }: CasterModifiersItemProps) {
     const toggleStyle = isPressed ? 'text-white' : 'text-[rgba(255,255,255,0.5)]';
 
-    return <div className={ `flex-1 text-center padding-2 bg-black bg-opacity-75 p-2 transition-all duration-200 ${toggleStyle}` }>
-        { children }
-    </div>
+    if (isPressed) {
+        return <div
+            className="w-1/4 text-center text-xl padding-2 bg-black bg-opacity-75 p-2 transition-all duration-200 text-white rounded-3xl"
+        >
+            { children }
+        </div>;
+    }
 }
