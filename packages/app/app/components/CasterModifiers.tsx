15fc4392a5dc35bc5a64065d20c9c9ee19d57a6c
ensi <66754841+ncor@users.noreply.github.com>
2023-11-23 13:14:01 +0300

    migrated, other changes

--- packages/app/app/components/CasterModifiers.tsx
+++ packages/app/app/components/CasterModifiers.tsx
@@ -1,33 +1,29 @@
 'use client';
 
-import { useContext, useEffect } from "react";
 import CasterModifiersItem from "./CasterModifiersItem";
-import { ModifiersContext } from "../contexts/modifiers";
-import { MODIFIERS } from "@/lib/keyboard/constants";
-import useKeystroke from "../hooks/keystroke";
+import { AnimatePresence, motion } from "framer-motion";
+import useModifiers from "../hooks/modifiers";
 
 
 export default function CasterModifiers() {
-    const keystroke = useKeystroke();
-    const modifiers = useContext(ModifiersContext);
+    const { modifiers } = useModifiers();
 
-    useEffect(() => {
-        if (!keystroke) return;
-
-        if (MODIFIERS.slice(0, 2).includes(keystroke.key))
-            modifiers.toggleShift(keystroke.isPressed);
-        if (MODIFIERS.slice(2, 4).includes(keystroke.key))
-            modifiers.toggleCtrl(keystroke.isPressed);
-        if (MODIFIERS.slice(4, 6).includes(keystroke.key))
-            modifiers.toggleAlt(keystroke.isPressed);
-        if (MODIFIERS.slice(6, 8).includes(keystroke.key))
-            modifiers.toggleMeta(keystroke.isPressed);
-    }, [ keystroke ]);
-
-    return <div className="caster-modifiers w-full flex gap-[2px] text-[1.25em] font-semibold">
-        <CasterModifiersItem isPressed={ modifiers.isShiftPressed }>⇧</CasterModifiersItem>
-        <CasterModifiersItem isPressed={ modifiers.isCtrlPressed }>⌃</CasterModifiersItem>
-        <CasterModifiersItem isPressed={ modifiers.isAltPressed }>⌥</CasterModifiersItem>
-        <CasterModifiersItem isPressed={ modifiers.isMetaPressed }>⌘</CasterModifiersItem>
-    </div>
+    return <AnimatePresence>
+        {
+            modifiers.isAnyModifierPressed &&
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.1 }}
+                className="w-full flex gap-[2px] font-semibold"
+            >
+                <CasterModifiersItem isPressed={ modifiers.isCapsActive }>⇪</CasterModifiersItem>
+                <CasterModifiersItem isPressed={ modifiers.isShiftPressed }>⇧</CasterModifiersItem>
+                <CasterModifiersItem isPressed={ modifiers.isCtrlPressed }>⌃</CasterModifiersItem>
+                <CasterModifiersItem isPressed={ modifiers.isAltPressed }>⌥</CasterModifiersItem>
+                <CasterModifiersItem isPressed={ modifiers.isMetaPressed }>⌘</CasterModifiersItem>
+            </motion.div>
+        }
+    </AnimatePresence>
 }
